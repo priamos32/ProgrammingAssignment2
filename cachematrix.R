d6848cc1a63ae26d7bf90e43bcd8759416838b49
priamos32 <32192476+priamos32@users.noreply.github.com>
2017-09-22 13:20:58 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The aim of these functions is to limit the numbers of inversion matrix calculations.
+## Once an inversion of a particular matrix is calculated, it's cached for further usage.
 
-## Write a short comment describing this function
+## The ouput of makeCacheMatrix function is the list of 4 functions. 
+## They allow to add and retreive the specific matrix and its inversion. 
+## By default, the inversion is set up as NULL.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+    inversion <- NULL
+    setmatrix <- function(x) {
+        matrix <<- x
+        inversion <<- matrix()
+    }
+    getmatrix <- function() matrix
+    setinversion <- function(x) inversion <<- x
+    getinversion <- function() inversion
+    list(setmatrix = setmatrix,
+         getmatrix = getmatrix,
+         setinversion = setinversion,
+         getinversion = getinversion)
 }
 
+## The aim of cacheSolve function is to calculate an inversion for a given matrix.
+## At the beginning, the function checks if the inversion was already calculated, i.e. inversion is not a null matrix.
+## If so, the inversion saved in cache is used. Otherwise, the inversion is calculated with solve() function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix, ...) {
+    inversion <- matrix$getinversion()
+    if(!is.null(inversion)) {
+        message("inversion cached")
+        return(inversion)
+    }
+    x <- matrix$getmatrix()
+    inversion <- solve(x)
+    matrix$setinversion(inversion)
+    inversion
 }
